35351ed5bff4a27c961e7dbc6eae1f80c48eeebb
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-11-08 22:32:20 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/quartz/HabrCareerParse.java
+++ src/main/java/quartz/HabrCareerParse.java
@@ -5,27 +5,33 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
-
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+    private static int PAGE_COUNT = 5;
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element dateElement = row.select(".vacancy-card__date").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            String date = dateElement.child(0).attr("datetime");
-            System.out.printf("%s %s %s%n ", vacancyName, link, date);
-        });
+
+        for (int i = 1; i <= PAGE_COUNT; i++) {
+            Connection connection = Jsoup.connect(PAGE_LINK + i);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element dateElement = row.select(".vacancy-card__date").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                String date = dateElement.child(0).attr("datetime");
+                LocalDateTime formattedDate = new HabrCareerDateTimeParser().parse(date);
+                System.out.printf("%s %s %s%n ", vacancyName, link, formattedDate);
+            });
+        }
     }
 }
--- src/main/java/utils/HabrCareerDateTimeParser.java
+++ src/main/java/utils/HabrCareerDateTimeParser.java
@@ -1,12 +1,15 @@
 package utils;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse("2016-10-31 23:59:59", DateTimeFormatter.ofPattern("yyyy-mm-dd“hh:mm:ss"));
+        return ZonedDateTime.parse(parse).toLocalDateTime();
     }
 }
 
